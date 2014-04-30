81123acd0c30cd59899705610ca97abfcb6cbd9a
hewolf <zhaokunl@andrew.cmu.edu>
2014-04-30 18:05:51 -0400

    forget password onclick working

--- src/com/az/ratedocs/ForgetPasswordActivity.java
+++ src/com/az/ratedocs/ForgetPasswordActivity.java
@@ -1,8 +1,11 @@
 package com.az.ratedocs;
 
+import com.az.ratedocs.onclick.OnClickFactory;
+import com.az.ratedocs.onclick.OnClickInterface;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.RequestPasswordResetCallback;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -14,56 +17,13 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class ForgetPasswordActivity extends Activity{
-	private EditText et_forgetpassword = null;
-	private Button btn_submitforgetpassword = null;
-	private String password = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_forget_password);
 		
-		et_forgetpassword = (EditText) findViewById(R.id.et_forgetpassword);
-		btn_submitforgetpassword = (Button) findViewById(R.id.btn_submitforgetpassword);
-		
-		btn_submitforgetpassword.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				password = et_forgetpassword.getText().toString();
-				checkEmailID();
-			}
-		});
+		@SuppressWarnings("unused")
+		OnClickInterface onClickInterface = OnClickFactory.getOnClick(this, ForgetPasswordActivity.this);
 	}
-	
-	protected void checkEmailID() {
-		if (TextUtils.isEmpty(password)) {
-			et_forgetpassword.setError(getString(R.string.error_field_required));
-		} else if (!password.contains("@")) {
-			et_forgetpassword.setError(getString(R.string.error_invalid_email));
-		}
-		else
-			forgotPassword(password);
-	}
-
-	public void forgotPassword(String email) {
-		//postEvent(new UserForgotPasswordStartEvent());
-		ParseUser.requestPasswordResetInBackground(email, new UserForgotPasswordCallback());
-	}
-	
-	private class UserForgotPasswordCallback extends RequestPasswordResetCallback{
-		public UserForgotPasswordCallback(){
-			super();
-		}
-		
-		@Override
-		public void done(ParseException e) {
-			if (e == null) {
-				Toast.makeText(getApplicationContext(), "Successfully sent link to your email for reset Password", Toast.LENGTH_LONG).show();
-			} else {
-				Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-			}
-		}		
-	}
-
 }
--- src/com/az/ratedocs/onclick/OnClickForgetPassword.java
+++ src/com/az/ratedocs/onclick/OnClickForgetPassword.java
@@ -1,22 +1,64 @@
 package com.az.ratedocs.onclick;
 
 import android.app.Activity;
-import com.az.ratedocs.utilities.StartIntent;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.az.ratedocs.R;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.RequestPasswordResetCallback;
 
 public class OnClickForgetPassword implements OnClickInterface {
-Activity activity;
-	
+	Activity activity;
+	private EditText et_forgetpassword = null;
+	private Button btn_submitforgetpassword = null;
+	private String password = null;
+
 	/* Associate the on click methods with our buttons */
 	public OnClickForgetPassword(Activity activity) {
 		this.activity = activity;
-//		Button button = (Button) activity.findViewById(R.id.add_lend_btn_my_books);
-//		button.setOnClickListener(new OnClickListener() {
-//
-//			@Override
-//			public void onClick(View arg0) {
-//				clickedAddLend();
-//			}
-//			
-//		});
+		et_forgetpassword = (EditText) activity.findViewById(R.id.et_forgetpassword);
+		btn_submitforgetpassword = (Button) activity.findViewById(R.id.btn_submitforgetpassword);
+		
+		btn_submitforgetpassword.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				password = et_forgetpassword.getText().toString();
+				checkEmailID();
+			}
+		});
+	}
+	protected void checkEmailID() {
+		if (TextUtils.isEmpty(password)) {
+			et_forgetpassword.setError(activity.getString(R.string.error_field_required));
+		} else if (!password.contains("@")) {
+			et_forgetpassword.setError(activity.getString(R.string.error_invalid_email));
+		}
+		else
+			forgotPassword(password);
+	}
+
+	public void forgotPassword(String email) {
+		ParseUser.requestPasswordResetInBackground(email, new UserForgotPasswordCallback());
+	}
+	
+	private class UserForgotPasswordCallback extends RequestPasswordResetCallback{
+		public UserForgotPasswordCallback(){
+			super();
+		}
+		
+		@Override
+		public void done(ParseException e) {
+			if (e == null) {
+				Toast.makeText(activity.getApplicationContext(), "Successfully sent link to your email for reset Password", Toast.LENGTH_LONG).show();
+			} else {
+				Toast.makeText(activity.getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+			}
+		}		
 	}
 }
